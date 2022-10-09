b29a0d281e8692e8200cac53aba79e07a8b7c923
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-10-09 12:03:42 +0500

    all is completed

--- contacts.py
+++ contacts.py
@@ -3,12 +3,12 @@ import csv
 _CONTACTS = {}
 
 
-def create_contact() -> dict:
+def create_contact():
     """
     Создает контакт
     """
-    name = input('Введите Имя и Фамилию (через пробел): ').split()
-    address = input('Введите адрес проживания: ')
+    name = input('Введите имя и фамилию (через пробел): ').split()
+    address = input('Введите адрес: ')
     phone = input('Введите номер телефона: ')
 
     contact = {
@@ -17,65 +17,70 @@ def create_contact() -> dict:
         'phone': phone
     }
 
-    return contact
-
-
-def add_contacts(contact: dict):
-    """Добавляет контакты в телефонный справочник"""
-    name = contact['name'][-1]
+    name = contact['name'][0]
     _CONTACTS[name] = contact
-    print(f'Контакт {name} успешно добавлен')
-
-
-def remove_contact(name: str):
-    """Удаляет контакт из телефонного справочника"""
-    contact = _CONTACTS.get(name)
-    if contact:
-        _CONTACTS.pop(name)
-        print(f'Контакт {name} успешно удален')
-    else:
-        print(f'Контакт {name} не сущестует в телефонной книге')
+    print(f'Контакт {name} добавлен в телефонную книгу')
 
 
 def view_contacts(name=None):
-    """Отображает контакты"""
+    """
+    Отображает контакты телефонной книги
+    """
     print('Контакты в телефонной книге: ')
 
     if not name:
-        lst_contacts = _CONTACTS.values()
+        contacts = _CONTACTS.values()
     else:
-        lst_contacts = [name]
+        contacts = [name]
     if _CONTACTS:
-        for item in lst_contacts:
+        for item in contacts:
             for contact in item.values():
                 print(contact)
     else:
-        print('Телефонная книга пустая')
+        print('Телефонная книга пуста')
 
 
 def find_contact(name: str):
-    """Ищет контакт в телефонном справочнике"""
+    """
+    Ищет контакт в телефонной книге по имени
+    """
     contact = _CONTACTS.get(name)
     if contact:
         view_contacts(contact)
     else:
-        print(f'Контакт {name} в телефонном справочнике не найден')
+        print(f'Контакт {name} не найден')
+
+
+def remove_contact(name: str):
+    """
+    Удаляет контакт из телефонной книги
+    """
+    contact = _CONTACTS.get(name)
+    if contact:
+        _CONTACTS.pop(name)
+        print(f'Контакт {name} удален')
+    else:
+        print(f'Контакт {name} не сущестует в телефонной книге')
 
 
 def export_contacts_txt():
-    """Экспортирует все контакты в текстовый файл"""
-    print('Телефонная книга успешна выгружена phone_book.txt файл')
+    """
+    Экспортирует телефонную книгу в файл формата txt
+    """
+    print('Телефонная книга сохранена в файл phone_book.txt')
     with open('phone_book.txt', 'w', encoding='utf-8') as f:
         for item in _CONTACTS.values():
-            tmp = []
+            contacts = []
             for contact in item.values():
-                tmp.append(contact)
-            print(*tmp, file=f)
+                contacts.append(contact)
+            print(*contacts, file=f)
 
 
 def export_contacts_csv():
-    """Экспортирует все контакты в csv файл"""
-    print('Телефонная книга успешна выгружена phone_book.csv файл')
+    """
+    Экспортирует телефонную книгу в файл формата csv
+    """
+    print('Телефонная книга сохранена в файл phone_book.csv')
     with open('phone_book.csv', 'w', encoding='utf-8') as f:
         csv_writer = csv.writer(f)
         for item in _CONTACTS.values():
--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 from view import show_menu
 from inputs import menu_item, request_contact
-from contacts import (create_contact, add_contacts, find_contact, remove_contact, view_contacts, export_contacts_csv,
+from contacts import (create_contact, find_contact, remove_contact, view_contacts, export_contacts_csv,
                       export_contacts_txt)
 
 
@@ -8,27 +8,24 @@ def phone_book():
     """
     Открывает меню телефонной книги
     """
-    run = True
-    contact = {}
-    while run:
+    show = True
+    while show:
         print('-' * 50)
         show_menu()
         menu_number = menu_item()
         if menu_number == 1:
-            contact = create_contact()
-        elif menu_number == 2 and contact:
-            add_contacts(contact)
-        elif menu_number == 3:
+            create_contact()
+        elif menu_number == 2:
             view_contacts()
-        elif menu_number == 4:
+        elif menu_number == 3:
             name = request_contact()
             find_contact(name)
-        elif menu_number == 5:
+        elif menu_number == 4:
             name = request_contact()
             remove_contact(name)
-        elif menu_number == 6:
+        elif menu_number == 5:
             export_contacts_txt()
-        elif menu_number == 7:
+        elif menu_number == 6:
             export_contacts_csv()
-        elif menu_number == 0:
-            run = False
+        elif menu_number == 7:
+            show = False
